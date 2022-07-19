f9f5cc631139862cee570a275419fa27770d1e59
haneesha25 <haneeshashaik25@gmail.com>
2022-07-18 20:31:33 -0400

    signout

--- lib/authentication/signOutMethod.dart
+++ lib/authentication/signOutMethod.dart
@@ -4,7 +4,9 @@ import 'package:firebase_auth/firebase_auth.dart';
 class SignOutMethod {
   FirebaseAuth _auth = FirebaseAuth.instance;
 
+
   Future<void> signOutUser() async {
     await _auth.signOut();
   }
 }
+
