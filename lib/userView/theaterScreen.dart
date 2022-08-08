fd62d07fca600d9c1c81d7c41deb27e3c99fc085
udayreddy20 <udaydheerajreddy@gmail.com>
2022-08-08 11:40:40 -0400

    theater screen changes

--- lib/userView/theaterScreen.dart
+++ lib/userView/theaterScreen.dart
@@ -1,5 +1,7 @@
 import 'package:cinemafinder/model/theaterModel.dart';
+import 'package:cinemafinder/userView/theaterCard.dart';
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class theaterScreen extends StatefulWidget {
   const theaterScreen({Key? key}) : super(key: key);
@@ -25,6 +27,61 @@ class _theaterScreenState extends State<theaterScreen> {
         ),
         centerTitle: true,
       ),
+      body: StreamBuilder(
+        stream: FirebaseFirestore.instance.collection('theater').snapshots(),
+        builder: (context,
+            AsyncSnapshot<QuerySnapshot<Map<String, dynamic>>> snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+          return ListView.builder(
+            itemCount: snapshot.data!.docs.length,
+            itemBuilder: (context, index) => theaterCard(
+              snap: snapshot.data!.docs[index].data(),
+            ),
+          );
+        },
+      ),
     );
   }
 }
+
+
+
+
+//       body: Container(
+//         child: Column(
+//           children: <Widget>[
+//             Container(
+//               padding: EdgeInsets.all(30),
+//               child: TextField(
+//                 controller: searchMovieController,
+//                 decoration: InputDecoration(
+//                   border: OutlineInputBorder(
+//                     borderSide: Divider.createBorderSide(context),
+//                   ),
+//                   filled: false,
+//                   labelText: 'search movies',
+//                   contentPadding: EdgeInsets.all(8),
+//                 ),
+//                 keyboardType: TextInputType.text,
+//               ),
+//             ),
+//             Expanded(
+//                 //  child: Center(),
+//                 child: ListView.builder(
+//                     itemCount: resultsMovies.length,
+//                     itemBuilder: (BuildContext context, int index) =>
+//                         theaterCard(snap: resultsMovies[index]))),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
+
+
+              
+              
