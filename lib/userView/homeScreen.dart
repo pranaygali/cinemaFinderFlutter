7ada3885e3014da43ae7370d8dc1a227096a9a0a
haneesha25 <haneeshashaik25@gmail.com>
2022-08-08 18:42:39 -0400

    homeScreen

--- lib/userView/homeScreen.dart
+++ lib/userView/homeScreen.dart
@@ -79,7 +79,6 @@ class _movieHomeScreenState extends State<movieHomeScreen> {
       resultsMovies = showMovies;
     });
   }
-
   getMovieStream() async {
     var movieData = await FirebaseFirestore.instance.collection('movie').get();
     setState(() {
@@ -90,104 +89,6 @@ class _movieHomeScreenState extends State<movieHomeScreen> {
     return "success";
   }
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        leading: Builder(builder: (context) {
-          return IconButton(
-            onPressed: () => Scaffold.of(context).openDrawer(),
-            icon: Icon(Icons.person),
-          );
-        } // context function
-            ),
-        backgroundColor: Colors.redAccent[400],
-        title: Text(
-          'Now showing',
-          style: TextStyle(
-              fontFamily: 'IbarraRealNova',
-              fontSize: 25,
-              fontWeight: FontWeight.bold),
-        ),
-        centerTitle: true,
-      ),
-      drawer: Drawer(
-        backgroundColor: Colors.redAccent[400],
-        child: ListView(
-          // Important: Remove any padding from the ListView.
-          padding: EdgeInsets.zero,
-          children: [
-            const DrawerHeader(
-              child: Center(
-                child: Text(
-                  'Entertainment4U',
-                  style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 35,
-                      color: Colors.white,
-                      fontFamily: 'IbarraRealNova'),
-                ),
-              ),
-            ),
-            ListTile(
-              leading: Icon(Icons.person, color: Colors.white),
-              title: Text(
-                '$fullName',
-                style: TextStyle(color: Colors.white),
-              ),
-              trailing: Icon(Icons.arrow_forward_ios, color: Colors.white),
-              onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => userProfile()),
-                );
-              },
-            ),
-            ListTile(
-              leading: Icon(
-                Icons.logout,
-                color: Colors.white,
-              ),
-              title: const Text(
-                'logout',
-                style: TextStyle(color: Colors.white),
-              ),
-              trailing: Icon(Icons.arrow_forward_ios, color: Colors.white),
-              onTap: () async {
-                await SignOutMethod().signOutUser();
-                Navigator.pushReplacement(context,
-                    MaterialPageRoute(builder: (context) => loginAcount()));
-              },
-            ),
-          ],
-        ),
-      ),
-      body: Container(
-        child: Column(
-          children: <Widget>[
-            Container(
-              padding: EdgeInsets.all(30),
-              child: TextField(
-                controller: searchMovieController,
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(
-                    borderSide: Divider.createBorderSide(context),
-                  ),
-                  filled: false,
-                  labelText: 'search movies',
-                  contentPadding: EdgeInsets.all(8),
-                ),
-                keyboardType: TextInputType.text,
-              ),
-            ),
-            Expanded(
-                //  child: Center(),
-                child: ListView.builder(
-                    itemCount: resultsMovies.length,
-                    itemBuilder: (BuildContext context, int index) =>
-                        movieCard(snap: resultsMovies[index]))),
-          ],
-        ),
       ),
     );
   }
