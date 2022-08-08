e3faeb79214b40bdb44293fe7e31c23886891c4b
haneesha25 <haneeshashaik25@gmail.com>
2022-08-08 18:29:58 -0400

    add movies

--- lib/admin/addMoviesMethod.dart
+++ lib/admin/addMoviesMethod.dart
@@ -9,60 +9,6 @@ import 'package:uuid/uuid.dart';
 class FireStoreMethods {
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
 
-  //upload the movie to the firebase
-
-  Future<String> uploadMovie(
-    // String movieName,
-    // String castDetails,
-    // String directorName,
-    // Uint8List file,
-    // String uid,
-
-    String name,
-    String cast,
-    String director,
-    Uint8List file,
-    String uid,
-
-    //  String checkId,
-  ) async {
-    String res = "An error occured";
-    try {
-      String photoUrl =
-          await ImageStorageMethods().uploadImage('moviePics', file, true);
-      // String movieid = const Uuid().v1();
-      final movieid = _firestore.collection('movie').doc();
-      // final randomid = _firestore.collection('movie').doc();
-      // Movie movie = Movie(
-      //   movieName: movieName,
-      //   castDetails: castDetails,
-      //   directorName: directorName,
-      //   movieId: movieId,
-      //   movieUrl: photoUrl,
-      // );
-      Movie movie = Movie(
-        name: name,
-        cast: cast,
-        director: director,
-        movieid: movieid.id,
-        // randomid: randomid.id,
-        imageUrl: photoUrl,
-      );
-
-      //   String checkId = _firestore
-      // .collection('movie')
-      // .document()
-      // .documentID;
-
-      _firestore.collection('movie').doc(movieid.id).set(movie.toJson());
-      // _firestore.collection('movie').add(movie.toJson());
-
-      res = "Movie uploaded";
-    } catch (err) {
-      res = err.toString();
-    }
-    return res;
-  }
 
   //Delete a movie
 
