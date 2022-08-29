98c854d4f4a3717612008c0687380e940cae2524
udayreddy20 <udaydheerajreddy@gmail.com>
2022-08-29 02:05:01 -0400

    changes

--- lib/admin/adminTheaterCard.dart
+++ lib/admin/adminTheaterCard.dart
@@ -1,11 +1,13 @@
 // This file deals with displaying of movies in the database in particular format
 import 'package:cinemafinder/admin/addMoviesMethod.dart';
 import 'package:cinemafinder/admin/addTheatersMethod.dart';
+import 'package:cinemafinder/admin/editTheaters.dart';
 import 'package:cinemafinder/userView/theaterScreen.dart';
 import 'package:flutter/material.dart';
 
 class adminTheaterCard extends StatelessWidget {
   final snap;
+
   const adminTheaterCard({Key? key, required this.snap}) : super(key: key);
 
   @override
@@ -13,64 +15,122 @@ class adminTheaterCard extends StatelessWidget {
     return Container(
       padding: EdgeInsets.only(top: 20, left: 10, right: 10, bottom: 20),
       width: double.infinity,
-      height: MediaQuery.of(context).size.height * 0.32,
+      height: MediaQuery.of(context).size.height * 0.23,
       child: Card(
         color: Colors.yellow,
         //  padding: EdgeInsets.all(50),
-        child: Container(
+        child:
+            // Container(
+            //   height: 270,
+            //   width: 150,
+            //   // width
+            //   // height: MediaQuery.of(context).size.height * 0.35,
+            //   child: Image.network(snap['movieUrl']),
+            // ),
+
+            Container(
           //  width: double.infinity,
           width: 210,
           child: Padding(
             padding: const EdgeInsets.only(
                 top: 10.0, left: 30, right: 30, bottom: 10),
             child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              //  mainAxisAlignment: MainAxisAlignment.spaceAround,
               crossAxisAlignment: CrossAxisAlignment.center,
               children: [
-                Text(
-                  snap['name'],
-                  style: TextStyle(
-                      fontFamily: 'IbarraRealNova',
-                      fontSize: 30,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.redAccent[400]),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      snap['name'],
+                      style: TextStyle(
+                          fontFamily: 'IbarraRealNova',
+                          fontSize: 30,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.redAccent[400]),
+                    ),
+                    IconButton(
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => editTheaters(
+                                theaterName: snap['name'],
+                                theaterAddress: snap['address'],
+                                theaterid: snap['theaterid'],
+                              ),
+                            ),
+                          );
+                        },
+                        icon: Icon(Icons.edit)),
+                  ],
                 ),
-                MaterialButton(
-                  minWidth: double.infinity,
-                  height: 50,
-                  onPressed: () {},
-                  elevation: 0,
-                  child: Text(
-                    'Edit',
-                    style: TextStyle(
-                        fontSize: 25,
-                        fontFamily: 'IbarraRealNova',
-                        fontWeight: FontWeight.bold,
-                        color: Colors.white),
-                  ),
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(20)),
-                  color: Colors.redAccent[400],
+
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      snap['address'],
+                      style: TextStyle(
+                          fontFamily: 'IbarraRealNova',
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.redAccent[400]),
+                    ),
+                    IconButton(
+                        onPressed: () async {
+                          FireStoreTheaterMethods()
+                              .deleteTheater(snap['theaterid']);
+                        },
+                        icon: Icon(Icons.delete_forever))
+                  ],
                 ),
-                MaterialButton(
-                  minWidth: double.infinity,
-                  height: 50,
-                  onPressed: () async {
-                    FireStoreTheaterMethods().deleteTheater(snap['theaterid']);
-                  },
-                  child: Text(
-                    'Delete',
-                    style: TextStyle(
-                        fontFamily: 'IbarraRealNova',
-                        fontSize: 25,
-                        color: Colors.white,
-                        fontWeight: FontWeight.bold),
-                  ),
-                  elevation: 0,
-                  color: Colors.redAccent[400],
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(20)),
-                ),
+
+                // MaterialButton(
+                //   minWidth: double.infinity,
+                //   height: 50,
+                //   onPressed: () async {
+                //     FireStoreTheaterMethods().deleteTheater(snap['theaterid']);
+                //   },
+                //   child: Text(
+                //     'Delete',
+                //     style: TextStyle(
+                //         fontFamily: 'IbarraRealNova',
+                //         fontSize: 25,
+                //         color: Colors.white,
+                //         fontWeight: FontWeight.bold),
+                //   ),
+                //   elevation: 0,
+                //   color: Colors.redAccent[400],
+                //   shape: RoundedRectangleBorder(
+                //       borderRadius: BorderRadius.circular(20)),
+                // ),
+                // MaterialButton(
+                //   minWidth: double.infinity,
+                //   height: 50,
+                //   onPressed: () {
+                //     Navigator.push(
+                //         context,
+                //         MaterialPageRoute(
+                //             builder: (context) => editTheaters(
+                //                   theaterName: snap['name'],
+                //                   theaterAddress: snap['address'],
+                //                   theaterid: snap['theaterid'],
+                //                 )));
+                //   },
+                //   elevation: 0,
+                //   child: Text(
+                //     'Edit',
+                //     style: TextStyle(
+                //         fontSize: 25,
+                //         fontFamily: 'IbarraRealNova',
+                //         fontWeight: FontWeight.bold,
+                //         color: Colors.white),
+                //   ),
+                //   shape: RoundedRectangleBorder(
+                //       borderRadius: BorderRadius.circular(20)),
+                //   color: Colors.redAccent[400],
+                // ),
               ],
             ),
           ),
